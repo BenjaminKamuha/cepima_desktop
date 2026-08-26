f817a9be48ca300bf29f44923654005482db3dd5
unknown <richardkivinga21@gmail.com>
2026-08-26 02:54:12 +0100

    Amélioration du formulaire d'affichage des patient

---
+++ AFW/Cepima/Cepima/MesUserCases/User_Dash_patient.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Cepima.MesUserCases
+{
+    partial class User_Dash_patient
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.customRoundedPanel2 = new CustomRoundedPanel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.customRoundedPanel1 = new CustomRoundedPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.customRoundedPanel3 = new CustomRoundedPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.customRoundedPanel4 = new CustomRoundedPanel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.customRoundedPanel2.SuspendLayout();
+            this.customRoundedPanel1.SuspendLayout();
+            this.customRoundedPanel3.SuspendLayout();
+            this.customRoundedPanel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // customRoundedPanel2
+            // 
+            this.customRoundedPanel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(123)))), ((int)(((byte)(229)))));
+            this.customRoundedPanel2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(123)))), ((int)(((byte)(229)))));
+            this.customRoundedPanel2.BorderRadius = 10;
+            this.customRoundedPanel2.BorderSize = 2;
+            this.customRoundedPanel2.Controls.Add(this.label4);
+            this.customRoundedPanel2.HoverBackColor = System.Drawing.Color.Empty;
+            this.customRoundedPanel2.HoverCursor = System.Windows.Forms.Cursors.Default;
+            this.customRoundedPanel2.Location = new System.Drawing.Point(10, 49);
+            this.customRoundedPanel2.Name = "customRoundedPanel2";
+            this.customRoundedPanel2.Size = new System.Drawing.Size(212, 100);
+            this.customRoundedPanel2.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(28, 14);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 15);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Total employés";
+            // 
+            // customRoundedPanel1
+            // 
+            this.customRoundedPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(123)))), ((int)(((byte)(229)))));
+            this.customRoundedPanel1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(123)))), ((int)(((byte)(229)))));
+            this.customRoundedPanel1.BorderRadius = 10;
+            this.customRoundedPanel1.BorderSize = 2;
+            this.customRoundedPanel1.Controls.Add(this.label1);
+            this.customRoundedPanel1.HoverBackColor = System.Drawing.Color.Empty;
+            this.customRoundedPanel1.HoverCursor = System.Windows.Forms.Cursors.Default;
+            this.customRoundedPanel1.Location = new System.Drawing.Point(273, 49);
+            this.customRoundedPanel1.Name = "customRoundedPanel1";
+            this.customRoundedPanel1.Size = new System.Drawing.Size(212, 100);
+            this.customRoundedPanel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(28, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Total employés";
+            // 
+            // customRoundedPanel3
+            // 
+            this.customRoundedPanel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(123)))), ((int)(((byte)(229)))));
+            this.customRoundedPanel3.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(123)))), ((int)(((byte)(229)))));
+            this.customRoundedPanel3.BorderRadius = 10;
+            this.customRoundedPanel3.BorderSize = 2;
+            this.customRoundedPanel3.Controls.Add(this.label2);
+            this.customRoundedPanel3.HoverBackColor = System.Drawing.Color.Empty;
+            this.customRoundedPanel3.HoverCursor = System.Windows.Forms.Cursors.Default;
+            this.customRoundedPanel3.Location = new System.Drawing.Point(536, 49);
+            this.customRoundedPanel3.Name = "customRoundedPanel3";
+            this.customRoundedPanel3.Size = new System.Drawing.Size(212, 100);
+            this.customRoundedPanel3.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(28, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Total employés";
+            // 
+            // customRoundedPanel4
+            // 
+            this.customRoundedPanel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(123)))), ((int)(((byte)(229)))));
+            this.customRoundedPanel4.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(123)))), ((int)(((byte)(229)))));
+            this.customRoundedPanel4.BorderRadius = 10;
+            this.customRoundedPanel4.BorderSize = 2;
+            this.customRoundedPanel4.Controls.Add(this.label3);
+            this.customRoundedPanel4.HoverBackColor = System.Drawing.Color.Empty;
+            this.customRoundedPanel4.HoverCursor = System.Windows.Forms.Cursors.Default;
+            this.customRoundedPanel4.Location = new System.Drawing.Point(799, 49);
+            this.customRoundedPanel4.Name = "customRoundedPanel4";
+            this.customRoundedPanel4.Size = new System.Drawing.Size(212, 100);
+            this.customRoundedPanel4.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(28, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 15);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Total employés";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(10, 202);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1001, 314);
+            this.panel1.TabIndex = 3;
+            // 
+            // User_Dash_patient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.customRoundedPanel4);
+            this.Controls.Add(this.customRoundedPanel3);
+            this.Controls.Add(this.customRoundedPanel1);
+            this.Controls.Add(this.customRoundedPanel2);
+            this.Name = "User_Dash_patient";
+            this.Size = new System.Drawing.Size(1021, 519);
+            this.customRoundedPanel2.ResumeLayout(false);
+            this.customRoundedPanel2.PerformLayout();
+            this.customRoundedPanel1.ResumeLayout(false);
+            this.customRoundedPanel1.PerformLayout();
+            this.customRoundedPanel3.ResumeLayout(false);
+            this.customRoundedPanel3.PerformLayout();
+            this.customRoundedPanel4.ResumeLayout(false);
+            this.customRoundedPanel4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CustomRoundedPanel customRoundedPanel2;
+        private System.Windows.Forms.Label label4;
+        private CustomRoundedPanel customRoundedPanel1;
+        private System.Windows.Forms.Label label1;
+        private CustomRoundedPanel customRoundedPanel3;
+        private System.Windows.Forms.Label label2;
+        private CustomRoundedPanel customRoundedPanel4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
